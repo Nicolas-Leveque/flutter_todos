b264aff291f29365be0fc15c74cafdda07274e56
Nicolas Lévêque <nicoleveque@outlook.com>
2023-02-06 17:46:21 +0100

    Creation de l'interface basique

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Todos Firebase',
+        home: Scaffold(
+          appBar: AppBar(
+              leading: Icon(Icons.ballot_rounded),
+              title: const Text('Todos Firebase')),
+        ));
+  }
+}
+
+class MyAppState extends ChangeNotifier {}
